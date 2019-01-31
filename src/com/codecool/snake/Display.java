fc13aab79e4fc291bbb82e0ea8c9b97ffedee09c
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-01-31 11:32:08 +0100

    colorized and positionized health title

--- src/com/codecool/snake/Display.java
+++ src/com/codecool/snake/Display.java
@@ -4,23 +4,29 @@ import com.codecool.snake.entities.GameEntity;
 import java.util.List;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 
 public class Display {
     private Pane displayPane;
     private DelayedModificationList<GameEntity> gameObjects = new DelayedModificationList<>();
-    private Text text = new Text(15,30,"Health: 100");
+    private Text text = new Text(80,20,"Health: 100");
 
 
     public Display(Pane pane) {
         displayPane = pane;
         displayPane.getChildren().add(text);
+        text.setFont(Font.font("Verdana", 20));
+        text.setFill(Color.DARKRED);
     }
 
     public void changeHealtTitle(String updatedHealth){
         displayPane.getChildren().remove(text);
-        text = new Text(15,30, updatedHealth);
+        text = new Text(80,20, updatedHealth);
+        text.setFont(Font.font ("Verdana", 20));
+        text.setFill(Color.DARKRED);
         displayPane.getChildren().add(text);
     }
 
